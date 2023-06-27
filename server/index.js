db82a334a1875271734a44549d71ff0b13f5f1c6
andrey.yurchenko <andrey.yurchenko1234@gmail.com>
2023-06-27 12:03:31 +0300

    server + client initialized

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
