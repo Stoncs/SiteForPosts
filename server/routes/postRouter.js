33ca6d22a99d5a548d3270aff2ed9ab84d89365a
andrey.yurchenko <andrey.yurchenko1234@gmail.com>
2023-07-02 06:03:35 +0300

    change route getByUserId

--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -3,7 +3,7 @@ const postController = require("../controllers/postController");
 const router = new Router();
 
 router.get("/", postController.getAll);
-router.get("/:userId", postController.getByUser);
+router.get("/getByUserId/:userId", postController.getByUser);
 router.get("/:id", postController.getById);
 
 module.exports = router;
